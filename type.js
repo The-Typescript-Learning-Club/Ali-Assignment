9d7638d025aa2d1dec22391517c5f324ee3888ee
muhammad ali <66193692+Muhammadali933@users.noreply.github.com>
2020-06-08 12:24:59 -0700

    Add files via upload

---
+++ type.js
@@ -0,0 +1,34 @@
+// FIRST PROGRAM 
+// BASIC SENTAX
+// string
+var msg = "hello world";
+console.log("hello world");
+// 2nd program
+// number
+var num1 = 1;
+console.log(num1);
+// 3rd program
+// boolean
+var isactive = true;
+console.log(isactive);
+// void
+function show() {
+    console.log("hello world");
+    document.write("hello world i am ali");
+}
+show();
+// null
+var a;
+a: null;
+// undefined
+var x;
+x: undefined;
+console.log(x);
+// arrary
+var list = [1, 2, 3, 4, 5];
+console.log(list);
+// touple
+var arr = [1, 2, 3, 4, 5];
+var tarr, string = [2, "muhammad"];
+console.log(arr);
+console.log(typeof (1));
---
+++ type.ts
@@ -0,0 +1,71 @@
+// FIRST PROGRAM 
+// BASIC SENTAX
+
+
+
+
+// string
+// A string is a data type.is used to represent text rather than numbers
+var msg:string="hello world";
+console.log("hello world");
+
+// 2nd program
+// number
+// Numeric data types are numbers stored in database columns.
+//  The exact numeric types are INTEGER , BIGINT , DECIMAL , NUMERIC ,number
+// example
+var num1:number=1;
+console.log(num1);
+
+
+// boolean
+//  the Boolean data type is a data type that has one of two possible values 
+//  (usually denoted true and false) 
+ var isactive:boolean=true;
+console.log(isactive);
+
+
+// void
+// void is used where there is no data type.
+// in return type of functions that do not return any value.
+function show(){
+    console.log("hello world");
+document.write("hello world i am ali");
+}
+show()
+
+// null
+// A null variable cannot contain other data types like number and string
+var a:null;
+a:null;
+console.log(a);
+
+
+
+// undefined
+//  undefined  have the values null and undefined
+//   By default, the type checker considers null and undefined
+var x:undefined;
+x:undefined;
+console.log(x);
+
+
+
+// arrary
+//   An array is a special type of data type.
+//  which can store multiple values of different data types.
+var list :number[]=[1,2,3,4,5];
+console.log(list);
+
+// touple
+// Tuple is a new data type which includes two set
+//  of values of different data types.
+
+var arr:number[]=[1,2,3,4,5];
+var tarr:number,string=[2,"muhammad"] ;
+console.log(arr);
+console.log(typeof(1));
+
+
+
+
